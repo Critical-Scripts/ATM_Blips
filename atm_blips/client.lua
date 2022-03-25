e469e6f1228f0f3d65da59ab5c337061052a364b
Tricky89 <102330237+Tricky89@users.noreply.github.com>
2022-03-24 18:26:52 -0700

    Add files via upload

---
+++ atm_blips/client.lua
@@ -0,0 +1,17 @@
+local showblips = true
+
+
+Citizen.CreateThread(function()
+	
+	if showblips then
+	  for k,v in ipairs(config.atms)do
+		local blip = AddBlipForCoord(v.x, v.y, v.z)
+		SetBlipSprite(blip, 500)
+		SetBlipScale(blip, 1.0)
+		SetBlipAsShortRange(blip, true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(tostring(v.name))
+		EndTextCommandSetBlipName(blip)
+	  end
+	end
+end)
---
+++ atm_blips/fxmanifest.lua
@@ -0,0 +1,9 @@
+author "Tricky"
+description "ATM Blips"
+version "1.0"
+
+fx_version "cerulean"
+game "gta5"
+
+shared_script "config.lua"
+client_script "client.lua"
