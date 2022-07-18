b19ef268e015035201e051daacf195342ec7c96b
Critical-Scripts <109039483+Critical-Scripts@users.noreply.github.com>
2022-07-17 18:16:50 -0700

    Update client.lua

--- atm_blips/client.lua
+++ atm_blips/client.lua
@@ -15,3 +15,17 @@ Citizen.CreateThread(function()
 	  end
 	end
 end)
+Citizen.CreateThread(function()
+	
+	if showblips then
+	  for k,v in ipairs(config.banks)do
+		local blip = AddBlipForCoord(v.x, v.y, v.z)
+		SetBlipSprite(blip, 100)
+		SetBlipScale(blip, 1.0)
+		SetBlipAsShortRange(blip, true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(tostring(v.name))
+		EndTextCommandSetBlipName(blip)
+	  end
+	end
+end)
